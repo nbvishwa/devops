8c2bbf4d1cf7123524744f5e77addae7563caabe
Vishwavijet <nbvishwa@gmail.com>
2021-09-10 09:07:57 +0530

    Update myproject.py

--- python/myproject.py
+++ python/myproject.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:black'>Hello There from python with CI/CD!</h1>"
+    return "<h1 style='color:black'>Hello there from python with CI/CD!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=true)
